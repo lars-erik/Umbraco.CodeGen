9d06ee5dcdd19ab80ca81be9db802a704fee4bb7
lars-erik <lea@markedspartner.no>
2013-10-13 02:19:17 +0200

    Barely started properties

---
+++ Umbraco.CodeGen.Tests/Parsers/PropertyParserTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Umbraco.CodeGen.Definitions;
+using Umbraco.CodeGen.Parsers;
+
+namespace Umbraco.CodeGen.Tests.Parsers
+{
+    public class PropertyParserTests : ContentTypeCodeParserTestBase
+    {
+        private const string SimplePropertyClass = @"
+                public class AClass {
+                    public string AProperty {get;set;}
+                }";
+        private GenericProperty property;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var codeGenConfig = new CodeGeneratorConfiguration();
+            Configuration = new ContentTypeConfiguration(codeGenConfig);
+            ContentType = new MediaType();
+            Parser = new PropertyParser(Configuration);
+        }
+
+        [Test]
+        public void Parse_PropertyNode_AddsGenericProperty()
+        {
+            ParseProperty(SimplePropertyClass, "AProperty");
+            Assert.AreEqual(1, ContentType.GenericProperties.Count);
+        }
+
+        [Test]
+        public void Parse_Alias_IsPropertyNameInCamelCase()
+        {
+            ParseProperty(SimplePropertyClass, "AProperty");
+            Assert.AreEqual("aProperty", property.Alias);
+        }
+
+        [Test]
+        public void Parse_Name_WhenPureProperty_IsSplitPascalCase()
+        {
+            const string code = SimplePropertyClass;
+            ParseProperty(code, "AProperty");
+            Assert.AreEqual("A Property", property.Name);
+        }
+
+        private void ParseProperty(string code, string propertyName)
+        {
+            var type = ParseType(code);
+            var prop = type.Members.SingleOrDefault(m => m.Name == propertyName);
+            Parser.Parse(prop, ContentType);
+            property = ContentType.GenericProperties.SingleOrDefault(p => p.Alias == propertyName.CamelCase());
+        }
+    }
+}
---
+++ Umbraco.CodeGen/Parsers/PropertyParser.cs
@@ -0,0 +1,23 @@
+﻿using ICSharpCode.NRefactory.CSharp;
+using Umbraco.CodeGen.Definitions;
+
+namespace Umbraco.CodeGen.Parsers
+{
+    public class PropertyParser : ContentTypeCodeParserBase
+    {
+        public PropertyParser(ContentTypeConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public override void Parse(AstNode node, ContentType contentType)
+        {
+            var propNode = (PropertyDeclaration) node;
+            var property = new GenericProperty
+            {
+                Alias = propNode.Name.CamelCase(),
+                Name = AttributeValue(propNode, "DisplayName", propNode.Name.SplitPascalCase())
+            };
+            contentType.GenericProperties.Add(property);
+        }
+    }
+}
