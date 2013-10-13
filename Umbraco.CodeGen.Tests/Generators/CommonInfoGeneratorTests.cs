ac6469efd1d5d02ebb5899a5f8de4282f83802f6
lars-erik <lea@markedspartner.no>
2013-10-13 14:00:13 +0200

    Code generation started

---
+++ Umbraco.CodeGen.Tests/Generators/CommonInfoGeneratorTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Umbraco.CodeGen.Definitions;
+using Umbraco.CodeGen.Generators;
+
+namespace Umbraco.CodeGen.Tests.Generators
+{
+    [TestFixture]
+    public class CommonInfoGeneratorTests
+    {
+        private CommonInfoGenerator generator;
+        private CodeTypeDeclaration type;
+
+        [SetUp]
+        public void SetUp()
+        {
+            generator = new CommonInfoGenerator(null);
+            type = new CodeTypeDeclaration();
+        }
+
+        [Test]
+        public void Generate_Alias_PascalCasesClassName()
+        {
+            var contentType = new MediaType {Info = {Alias = "aClass"}};
+            Generate(contentType);
+            Assert.AreEqual("AClass", type.Name);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception), ExpectedMessage = "Cannot generate class with alias null or empty")]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Generate_Alias_NullOrEmpty_Throws(string alias)
+        {
+            var contentType = new MediaType {Info={Alias=alias}};
+            Generate(contentType);
+        }
+
+        [Test]
+        public void Generate_Name_NotEqualToAlias_AddsDisplayNameAttribute()
+        {
+            var contentType = new MediaType {Info = {Alias = "aClass", Name = "A fancy class"}};
+            Generate(contentType);
+            Assert.AreEqual("A fancy class", FindAttributeValue("DisplayName"));
+        }
+
+        [Test]
+        [TestCase("A Class")]
+        [TestCase("AClass")]
+        [TestCase("aClass")]
+        public void Generate_Name_LowerCaseOrSplitEqualsAlias_DoesNotAddDisplayNameAttribute(string name)
+        {
+            var contentType = new MediaType {Info = {Alias = "aClass", Name = name}};
+            Generate(contentType);
+            Assert.AreEqual(0, type.CustomAttributes.Count);
+        }
+
+        [Test]
+        public void Generate_Description_WhenNonEmpty_AddsDescriptionAttribute()
+        {
+            var contentType = new MediaType {Info = {Alias = "aClass", Description = "A fancy class"}};
+            Generate(contentType);
+            Assert.AreEqual("A fancy class", FindAttributeValue("Description"));
+        }
+
+        private void Generate(ContentType contentType)
+        {
+            generator.Generate(type, contentType);
+        }
+
+        private object FindAttributeValue(string attributeName)
+        {
+            var attribute = type.CustomAttributes.Cast<CodeAttributeDeclaration>().Single(att => att.Name == attributeName);
+            var value = ((CodePrimitiveExpression) attribute.Arguments[0].Value).Value;
+            return value;
+        }
+    }
+}
---
+++ Umbraco.CodeGen/Generators/CommonInfoGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.CodeDom;
+using Umbraco.CodeGen.Definitions;
+
+namespace Umbraco.CodeGen.Generators
+{
+    public class CommonInfoGenerator
+    {
+        private const StringComparison IgnoreCase = StringComparison.OrdinalIgnoreCase;
+
+        public CommonInfoGenerator(ContentTypeConfiguration config)
+        {
+            
+        }
+
+        public void Generate(CodeObject codeObject, ContentType contentType)
+        {
+            var type = (CodeTypeDeclaration) codeObject;
+            var info = contentType.Info;
+
+            // TODO: Consider moving these to base for both info & props
+            ValidateAlias(info);
+            SetAlias(type, info);
+            AddDisplayNameIfDifferent(type, info);
+            AddDescription(type, info);
+
+        }
+
+        private static void SetAlias(CodeTypeMember type, EntityDescription info)
+        {
+            type.Name = info.Alias.PascalCase();
+        }
+
+        private static void ValidateAlias(Info info)
+        {
+            if (String.IsNullOrWhiteSpace(info.Alias))
+                throw new Exception("Cannot generate class with alias null or empty");
+        }
+
+        private static void AddDisplayNameIfDifferent(CodeTypeMember type, EntityDescription info)
+        {
+            if (String.Compare(info.Name, info.Alias, IgnoreCase) == 0 &&
+                String.Compare(info.Name, info.Alias.SplitPascalCase(), IgnoreCase) == 0)
+                return;
+            type.CustomAttributes.Add(
+                new CodeAttributeDeclaration(
+                    "DisplayName",
+                    new CodeAttributeArgument(
+                        new CodePrimitiveExpression(info.Name)
+                        )
+                    )
+                );
+        }
+
+        private void AddDescription(CodeTypeDeclaration type, Info info)
+        {
+            if (String.IsNullOrWhiteSpace(info.Description))
+                return;
+
+
+        }
+    }
+}
