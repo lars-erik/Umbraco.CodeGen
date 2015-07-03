41613ca6fe1f589e791bf5ef5277b187c1ee1886
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2015-07-03 20:51:35 +0200

    Maps document type definition from Umbraco ContentType

--- Umbraco.CodeGen.Umbraco.Tests/Mapping_IContentType_To_ContentType.cs
+++ Umbraco.CodeGen.Umbraco.Tests/Mapping_IContentType_To_ContentType.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using Moq;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using Umbraco.CodeGen.Definitions;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Configuration.UmbracoSettings;
 using Umbraco.Core.Models;
+using ContentType = Umbraco.Core.Models.ContentType;
 
 namespace Umbraco.CodeGen.Umbraco.Tests
 {
@@ -16,52 +19,23 @@ namespace Umbraco.CodeGen.Umbraco.Tests
         [Test]
         public void Maps_Metadata()
         {
-            var umbct = new Core.Models.ContentType(-1)
-            {
-                Alias = "SomeDocumentType",
-                Name = "Some document type",
-                AllowedAsRoot = true,
-                AllowedContentTypes = new[] { new ContentTypeSort(new Lazy<int>(() => 1), 1, "SomeOtherDocType"), },
-                AllowedTemplates = new[] { new Template("", "", "ATemplate"), new Template("", "", "AnotherTemplate"), },
-                PropertyGroups = new PropertyGroupCollection(new[]{
-                    new PropertyGroup
-                    {
-                      Name  = "A tab",
-                      PropertyTypes = new PropertyTypeCollection(new []
-                      {
-                          new PropertyType(new DataTypeDefinition(-1, "Umbraco.Richtext"), "Richtext editor")
-                          {
-                              Alias = "someProperty",
-                              Description = "A description"
-                          }, 
-                          new PropertyType(new DataTypeDefinition(-1, "Umbraco.Richtext"), "Richtext editor")
-                          {
-                              Alias = "anotherProperty",
-                              Description = "Another description"
-                          } 
-                      })
-                    },
-                    new PropertyGroup(new PropertyTypeCollection(new []
-                    {
-                          new PropertyType(new DataTypeDefinition(-1, "Umbraco.Number"), "Numeric")
-                          {
-                              Alias = "tablessProperty"
-                          },
-                    }))
-                }),
-                Description = "A description of some document type",
-                Icon = "privateMemberIcon.gif",
-                Thumbnail = "privateMemberThumb.png",
-            };
-            umbct.SetDefaultTemplate(umbct.AllowedTemplates.First());
+            var expected = CreateCodeGenDocumentType();
+            var umbracoContentType = CreateUmbracoContentType();
 
-            var expected = new Definitions.DocumentType
+            var actual = ContentTypeMapping.Map(umbracoContentType);
+
+            Assert.AreEqual(expected, actual, Serialize(actual));
+        }
+
+        private static DocumentType CreateCodeGenDocumentType()
+        {
+            var expected = new DocumentType
             {
                 Info = new DocumentTypeInfo
                 {
                     Alias = "SomeDocumentType",
                     AllowAtRoot = true,
-                    AllowedTemplates = new List<string> { "ATemplate", "AnotherTemplate" },
+                    AllowedTemplates = new List<string> {"ATemplate", "AnotherTemplate"},
                     DefaultTemplate = "ATemplate",
                     Description = "A description of some document type",
                     Icon = "privateMemberIcon.gif",
@@ -71,7 +45,8 @@ namespace Umbraco.CodeGen.Umbraco.Tests
                 },
                 Tabs = new List<Tab>
                 {
-                    new Tab{Caption="A tab"}
+                    new Tab {Caption = "A tab"},
+                    new Tab()
                 },
                 Structure = new List<string>
                 {
@@ -82,7 +57,7 @@ namespace Umbraco.CodeGen.Umbraco.Tests
                     new GenericProperty
                     {
                         Alias = "someProperty",
-                        Definition = "Richtext editor",
+                        Definition = null,
                         Description = "A description",
                         Name = "Some property",
                         Tab = "A tab",
@@ -91,7 +66,7 @@ namespace Umbraco.CodeGen.Umbraco.Tests
                     new GenericProperty
                     {
                         Alias = "anotherProperty",
-                        Definition = "Richtext editor",
+                        Definition = null,
                         Description = "Another description",
                         Name = "Another property",
                         Tab = "A tab",
@@ -100,18 +75,82 @@ namespace Umbraco.CodeGen.Umbraco.Tests
                     new GenericProperty
                     {
                         Alias = "tablessProperty",
-                        Definition = "Numeric",
+                        Definition = null,
                         Name = "Tabless property",
-                        Tab = "",
                         Type = "Umbraco.Number"
                     },
                 }
             };
+            return expected;
+        }
 
-            var actual = ContentTypeMapping.Map(umbct);
+        private static ContentType CreateUmbracoContentType()
+        {
+            var umbracoContentType = new Core.Models.ContentType(-1)
+            {
+                Alias = "SomeDocumentType",
+                Name = "Some document type",
+                AllowedAsRoot = true,
+                AllowedContentTypes = new[] {new ContentTypeSort(new Lazy<int>(() => 1), 1, "SomeOtherDocType"),},
+                AllowedTemplates = new[] {new Template("", "", "ATemplate"), new Template("", "", "AnotherTemplate"),},
+                PropertyGroups = new PropertyGroupCollection(new[]
+                {
+                    new PropertyGroup
+                    {
+                        Name = "A tab",
+                        PropertyTypes = new PropertyTypeCollection(new[]
+                        {
+                            new PropertyType(new DataTypeDefinition(-1, "Umbraco.Richtext"), "Richtext editor")
+                            {
+                                Alias = "someProperty",
+                                Name = "Some property",
+                                Description = "A description"
+                            },
+                            new PropertyType(new DataTypeDefinition(-1, "Umbraco.Richtext"), "Richtext editor")
+                            {
+                                Alias = "anotherProperty",
+                                Name = "Another property",
+                                Description = "Another description"
+                            }
+                        })
+                    },
+                    new PropertyGroup(new PropertyTypeCollection(new[]
+                    {
+                        new PropertyType(new DataTypeDefinition(-1, "Umbraco.Number"), "Numeric")
+                        {
+                            Alias = "tablessProperty",
+                            Name = "Tabless property"
+                        },
+                    }))
+                }),
+                Description = "A description of some document type",
+                Icon = "privateMemberIcon.gif",
+                Thumbnail = "privateMemberThumb.png",
+            };
+            umbracoContentType.SetDefaultTemplate(umbracoContentType.AllowedTemplates.First());
+            return umbracoContentType;
+        }
 
-            Assert.AreEqual(expected, actual, JsonConvert.SerializeObject(actual));
+        private static string Serialize(object obj)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
+        }
 
+        [SetUp]
+        public void Setup()
+        {
+            var settings = new Mock<IUmbracoSettingsSection>();
+            var requestHandler = new Mock<IRequestHandlerSection>();
+            settings.Setup(m => m.RequestHandler).Returns(requestHandler.Object);
+            requestHandler.Setup(m => m.CharCollection).Returns(new IChar[0]);
+            SetUmbracoSettings(settings.Object);
+        }
+
+        private static void SetUmbracoSettings(IUmbracoSettingsSection umbracoSettingsSection)
+        {
+            var type = typeof (UmbracoConfig);
+            var method = type.GetMethod("SetUmbracoSettings", BindingFlags.Instance | BindingFlags.NonPublic);
+            method.Invoke(UmbracoConfig.For, new object[] {umbracoSettingsSection});
         }
     }
 }
