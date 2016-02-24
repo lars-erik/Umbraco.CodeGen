0811aad8390cb14c3c82a67bfdf8dfcb1bdc8035
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-02-24 23:51:22 +0100

    Missing file

---
+++ Umbraco.CodeGen/Factories/SimpleModelGeneratorFactory.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.CodeGen.Generators;
+using Umbraco.CodeGen.Generators.Compositions;
+
+namespace Umbraco.CodeGen.Factories
+{
+    public class SimpleModelGeneratorFactory : CodeGeneratorFactory
+    {
+        public override CodeGeneratorBase Create(Configuration.GeneratorConfig config)
+        {
+            return new NamespaceGenerator(config,
+                new ImportsGenerator(config),
+                new SimpleClassGenerator(config,
+                    new PropertiesGenerator(config,
+                        new SimplePropertyGenerator(config)
+                    )
+                )
+            );
+        }
+    }
+}
