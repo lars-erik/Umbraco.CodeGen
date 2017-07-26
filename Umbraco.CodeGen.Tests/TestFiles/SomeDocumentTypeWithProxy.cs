190787a46541e2a01566327b03200bf37dea666f
Lars-Erik Aabech <lea@markedspartner.no>
2017-07-26 17:51:59 +0200

    Gold file for proxied adapter.

---
+++ Umbraco.CodeGen.Tests/TestFiles/SomeDocumentTypeWithProxy.cs
@@ -0,0 +1,44 @@
+﻿namespace Umbraco.CodeGen.Models
+{
+    using global::System;
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Web;
+
+    public partial class SomeDocumentType : global::Umbraco.Core.Models.PublishedContent.PublishedContentModel
+    {
+        Umbraco.CodeGen.Pocos.SomeDocumentType proxy;
+        public Umbraco.CodeGen.Pocos.SomeDocumentType Proxy
+        {
+            get { return proxy; }
+        }
+        public SomeDocumentType(IPublishedContent content) :
+            base(content)
+        {
+            proxy = Umbraco.CodeGen.Pocos.SomeDocumentType();
+            proxy.SomeProperty = SomeProperty;
+            proxy.AnotherProperty = AnotherProperty;
+            proxy.TablessProperty = TablessProperty;
+        }
+        public virtual System.Web.IHtmlString SomeProperty
+        {
+            get
+            {
+                return Content.GetPropertyValue<System.Web.IHtmlString>("someProperty");
+            }
+        }
+        public virtual System.Web.IHtmlString AnotherProperty
+        {
+            get
+            {
+                return Content.GetPropertyValue<System.Web.IHtmlString>("anotherProperty");
+            }
+        }
+        public virtual int TablessProperty
+        {
+            get
+            {
+                return Content.GetPropertyValue<int>("tablessProperty");
+            }
+        }
+    }
+}
